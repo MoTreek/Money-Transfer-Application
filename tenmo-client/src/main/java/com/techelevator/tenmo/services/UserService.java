20c6d56d3e5ece3d43f6e592458918dae7439ad0
Garrett Kubacki <gjk5032@gmail.com>
2022-07-06 12:31:04 -0400

    you up

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/UserService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/UserService.java
@@ -1,5 +1,10 @@
 package com.techelevator.tenmo.services;
 
+import com.techelevator.tenmo.model.User;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 public class UserService {
@@ -10,6 +15,31 @@ public class UserService {
 
     public void setAuthToken(String authToken){
         this.authToken = authToken;
+    }
+
+    /**
+     * Find a single user by user ID.
+     */
+    public User[] user() {
+        User[] user = null;
+        try {
+            ResponseEntity<User[]> response =
+                    restTemplate.exchange(API_BASE_URL + "users/",
+                            HttpMethod.GET, makeAuthEntity(), User[].class);
+            user = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return user;
+    }
+
+    /**
+     * Returns an HttpEntity with the `Authorization: Bearer:` header
+     */
+    private HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(headers);
     }
 
 
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDAO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDAO.java
@@ -1,4 +1,33 @@
 package com.techelevator.tenmo.dao;
 
-public class JdbcAccountDAO {
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class JdbcAccountDAO implements AccountDAO {
+
+    private static JdbcTemplate jdbcTemplate;
+    private BigDecimal balance;
+
+    public void JdbcUserDao(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
+
+    @Override
+    public Account get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Account> list() {
+        return null;
+    }
+
+    @Override
+    public Account update(Account account) {
+        return null;
+    }
+
+    //NOT DONE YET
+
 }
