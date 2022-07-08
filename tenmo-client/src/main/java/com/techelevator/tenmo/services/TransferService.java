0578616f3f00ae45053cbcf34af4f684915b6b7a
Garrett Kubacki <gjk5032@gmail.com>
2022-07-08 14:36:09 -0400

    ;laskdfl;asjfd

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,13 +1,18 @@
 package com.techelevator.tenmo.services;
 
 import com.techelevator.tenmo.model.Transfer;
+import org.apiguardian.api.API;
+import org.springframework.http.ResponseEntity;
 
 public class TransferService extends AuthenticatedAPIService {
     public TransferService() {
     }
 
-    public Transfer createTransfer() {
-        return null;
+    public Transfer createTransfer(Transfer transfer) {
+Transfer transfer = null;
+try {
+    ResponseEntity<Transfer> response = restTemplate.exchange(API_BASE_URL + "transfers/")
+}
     }
 
     public Transfer updatePendingTransferStatus() {
