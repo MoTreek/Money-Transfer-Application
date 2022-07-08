2a166d1f631d47981886c00717852b60bb168562
Garrett Kubacki <gjk5032@gmail.com>
2022-07-08 14:39:29 -0400

    masldf

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -8,12 +8,8 @@ public class TransferService extends AuthenticatedAPIService {
     public TransferService() {
     }
 
-    public Transfer createTransfer(Transfer transfer) {
-Transfer transfer = null;
-try {
-    ResponseEntity<Transfer> response = restTemplate.exchange(API_BASE_URL + "transfers/")
-}
-    }
+    public Transfer createTransfer() {
+
 
     public Transfer updatePendingTransferStatus() {
         return null;
