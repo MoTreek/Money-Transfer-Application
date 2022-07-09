8850c2b5a51fb1be2c463de88d4ff8e413fc807a
Mohamed Treek <Mohamedtreek0@gmail.com>
2022-07-09 13:05:16 -0400

    Looking for bugs

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
@@ -5,9 +5,10 @@ import com.techelevator.tenmo.model.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class UserController {
     private UserDao userdao;
 
@@ -16,7 +17,9 @@ public class UserController {
     }
 
     @RequestMapping(path = "/users/", method = RequestMethod.GET)
-    public List<User> findAll() {return userdao.findAll();}
+    public List<User> findAll() {
+        return userdao.findAll();
+    }
 
     @RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
     public String getUserById(@PathVariable int id) {return userdao.getUserById(id);}
